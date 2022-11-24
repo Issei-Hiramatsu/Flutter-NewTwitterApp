068251340d2f7902767967f7c365e880883b67d8
issei <issei.hiramatsu@gmail.com>
2022-11-24 16:52:14 +0900

    git update add BottomTabBar  view/notification

--- lib/view/notification/notification_page.dart
+++ lib/view/notification/notification_page.dart
@@ -3,44 +3,83 @@ import 'package:flutter/material.dart';
 import '../../component/app_bar/sliver_app_bar.dart';
 import '../../component/bottom_navigation_bar.dart';
 
-class MessagePage extends StatelessWidget {
+//TODO 変更した色をもとに戻す
+
+class _SliverTabBarDelegate extends SliverPersistentHeaderDelegate {
+  const _SliverTabBarDelegate({
+    required this.tabBar,
+  });
+
+  final TabBar tabBar;
+
+  @override
+  double get minExtent => tabBar.preferredSize.height;
+  @override
+  double get maxExtent => tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverTabBarDelegate oldDelegate) {
+    return false;
+  }
+}
+
+class NotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white, //statusbarと色が被っているため
-      body: DefaultTabController(
-          length: 3,
-          child: NestedScrollView(
-            headerSliverBuilder:
-                (BuildContext context, bool innerBoxIsScrolled) {
-              return <Widget>[
-                TwitterSliverAppBar(
-                  titleSpace: const Text('通知'),
-                  iconSpace: Icons.settings_outlined,
-                  iconAction: () {},
-                  bottomSpace: const TabBar(tabs: [
-                    Tab(text: '野球'),
-                    Tab(text: 'サッカー'),
-                    Tab(text: 'テニス'),
-                  ]),
-                ),
-              ];
-            },
-            body: const TabBarView(
-              children: [
-                //Tabごとにウィジェットを設定する
-                Center(
-                  child: Text('野球', style: TextStyle(fontSize: 32.0)),
-                ),
-                Center(
-                  child: Text('サッカー', style: TextStyle(fontSize: 32.0)),
-                ),
-                Center(
-                  child: Text('テニス', style: TextStyle(fontSize: 32.0)),
-                ),
-              ],
-            ),
-          )),
+      body: SafeArea(
+        bottom: false,
+        child: DefaultTabController(
+            length: 3,
+            child: NestedScrollView(
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return <Widget>[
+                  TwitterSliverAppBar(
+                    titleSpace:
+                        const Text('通知', style: TextStyle(color: Colors.black)),
+                    iconSpace: Icons.settings_outlined,
+                    iconAction: () {},
+                  ),
+                  const SliverPersistentHeader(
+                      pinned: true,
+                      delegate: _SliverTabBarDelegate(
+                          tabBar: TabBar(
+                        labelColor: Colors.black,
+                        indicatorColor: Colors.blue,
+                        tabs: [
+                          Tab(text: 'すべて'),
+                          Tab(text: '認証済み'),
+                          Tab(text: '@ツイート'),
+                        ],
+                      ))),
+                ];
+              },
+              body: const TabBarView(
+                children: [
+                  //TODO Tabごとにウィジェットを設定する
+                  Center(
+                    child: Text('', style: TextStyle(fontSize: 32.0)),
+                  ),
+                  Center(
+                    child: Text('', style: TextStyle(fontSize: 32.0)),
+                  ),
+                  Center(
+                    child: Text('', style: TextStyle(fontSize: 32.0)),
+                  ),
+                ],
+              ),
+            )),
+      ),
       bottomNavigationBar: TwitterBottomNavigationBar(),
     );
   }
